40e5300e86363460a556a92e74290c9f2fb071af
Markeu11 <marklesther1130@gmail.com>
2024-09-28 11:23:04 +0800

    message

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './App.css'; 
+
+function App() { 
+  return ( 
+    <div className="App"> 
+      <h1>Hello, World!</h1> {/* Added string here */}
+    </div> 
+  ); 
+} 
+
+export default App; 
